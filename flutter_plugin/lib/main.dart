be430b0bbb1b8e8ce81621e73229e35d9159ae1a
champaksworld <champaksworld@users.noreply.github.com>
2021-03-31 20:51:42 +0530

    Dark Theme

--- flutter_plugin/lib/main.dart
+++ flutter_plugin/lib/main.dart
@@ -6,6 +6,7 @@ class VSJStateless extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme:ThemeData.dark(),
       home: VSJStateful(),
     );
   }
