e9df14a6fc69159efb65f311b332b30714147119
champaksworld <champaksworld@users.noreply.github.com>
2021-04-03 20:45:50 +0530

    Simple Row Column Division

--- flutter_plugin/lib/VSJStateful.dart
+++ flutter_plugin/lib/VSJStateful.dart
@@ -13,18 +13,38 @@ class _VSJStatefulState extends State<VSJStateful> {
         centerTitle: true,
         title: Text('VSJ Theming'),
       ),
-      body: Center(
-        child: Text('The Body'),
-      ),
-      floatingActionButton: Theme(
-        data: ThemeData(
-          accentColor: Colors.red,
-
+      body: Container(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: Text("1,1"),
+                ),
+                Expanded(
+                  child: Text("1,2"),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Text("2,1"),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Text("1,1"),
+                ),
+                Expanded(
+                  child: Text("1,2"),
+                )
+              ],
+            ),
+          ],
         ),
-        child: FloatingActionButton(
-          child: Icon(Icons.add),
-        ),
-
       ),
     );
   }
